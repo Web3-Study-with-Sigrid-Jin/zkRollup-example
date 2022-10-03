77067694776d3f70db0bc0d87eaf1bcec4b91987
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-10-03 13:11:54 +0900

    chore: the example of circom when bad comparator goes to unknown

--- 4_comparator/test.sh
+++ 4_comparator/test.sh
@@ -1,7 +1,9 @@
 set -e
 
+# Although circom’s parser (see parser/jaz.jison) supports if statements, because circom compiles the DSL into arithmetic circuits, circuits whose behavior depends on the value of an input can have unexpected behavior.
 echo "compiling good"
 circom force_equal_if_enabled.circom --r1cs --wasm --sym
 
+# There are constraints depending on the value of the condition and it can be unknown during the constraint generation phase
 echo "compiling bad"
 circom bad_force_equal_if_enabled.circom --r1cs --wasm --sym
